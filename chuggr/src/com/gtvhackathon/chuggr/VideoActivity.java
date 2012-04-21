11aeaa5875acfc5d570653d4bd362b543de7f32c
Matthew Rollings <github@stealthcopter.com>
2012-04-21 12:25:02 +0100

    First commit of project

---
+++ chuggr/src/com/gtvhackathon/chuggr/VideoActivity.java
@@ -0,0 +1,15 @@
+package com.gtvhackathon.chuggr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VideoActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
