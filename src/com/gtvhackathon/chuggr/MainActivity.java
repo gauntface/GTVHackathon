03ca6d572de9c480d2a869ae15ab553d86c26071
Matthew Rollings <github@stealthcopter.com>
2012-04-21 14:11:42 +0100

    renamed and holoed

--- src/com/gtvhackathon/chuggr/MainActivity.java
+++ src/com/gtvhackathon/chuggr/MainActivity.java
@@ -10,7 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class VideoActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
